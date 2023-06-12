715f5ffa87b2dc80e474c9eb53ccfe761fa2b942
pp449 <mma7710@naver.com>
2023-06-12 16:08:35 +0900

    Config: @types 디렉토리 절대경로 추가

--- jest.config.js
+++ jest.config.js
@@ -13,6 +13,7 @@ const customJestConfig = {
     '^@components/(.*)$': '<rootDir>/src/components/$1',
     '^@pages/(.*)$': '<rootDir>/src/pages/$1',
     '^@mocks/(.*)$': '<rootDir>/__mocks__/$1',
+    '^@type/(.*)$': '<rootDir>/src/@types/$1',
   },
 };
 
