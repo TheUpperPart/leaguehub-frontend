25fce634871dc7028e94a46c243ffaa1607bb6f6
pp449 <mma7710@naver.com>
2023-06-15 15:04:01 +0900

    Test: 테스트에 사용되는 서버 URL 을 모듈화된 변수를 가져와 사용하도록 변경

--- __mocks__/handlers/testHandlers.ts
+++ __mocks__/handlers/testHandlers.ts
@@ -1,7 +1,8 @@
 import { rest } from 'msw';
+import { SERVER_URL } from '@config/index';
 
 const testHandlers = [
-  rest.get('/test', (req, res, ctx) => {
+  rest.get(SERVER_URL + '/test', (req, res, ctx) => {
     return res(ctx.json('test'));
   }),
 ];
