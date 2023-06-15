d797241a90e186b21643432f5c557a28c0f74ab7
pp449 <mma7710@naver.com>
2023-06-15 14:58:17 +0900

    Config: 개발 환경에서 MSW 실행되도록 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,11 @@
 import Layout from '@components/layout';
 import initMockAPI from '@mocks/index';
 import type { AppProps } from 'next/app';
+if (process.env.NODE_ENV === 'development') {
+  initMockAPI();
+}
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  // if (process.env.NODE_ENV === 'development') {
-  //   initMockAPI();
-  // }
   return (
     <Layout>
       <Component {...pageProps} />;
