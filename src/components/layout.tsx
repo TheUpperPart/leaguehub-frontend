fba58d0e81433773f9e2905aa46b57226cd68d6b
pp449 <mma7710@naver.com>
2023-06-17 16:45:35 +0900

    Feat: 채널 사이드바를 페이지에 넣음

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,29 @@
+import ChannelBar from '@components/Sidebar/ChannelBar/ChannelBar';
+import { SERVER_URL } from '@config/index';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import { PropsWithChildren } from 'react';
 import GlobalStyle from 'src/styles/GlobalStyle';
 
+const fetchData = async () => {
+  const response = await axios.get(SERVER_URL + '/api/channels', {
+    headers: {
+      Authorization: 'User Token',
+    },
+  });
+
+  return response.data;
+};
+
 const Layout = ({ children }: PropsWithChildren) => {
+  const { data } = useQuery(['getChannels'], fetchData, {
+    staleTime: Infinity,
+    cacheTime: Infinity,
+  });
   return (
     <>
       <GlobalStyle />
+      <ChannelBar ChannelCircles={data} />
       <main>{children}</main>
     </>
   );
