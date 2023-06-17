4a3577a0f3d68e9b19433adf407d20aed0a62f28
navyjeongs <wjdtjr8649@naver.com>
2023-06-17 17:03:13 +0900

    Feat: layout에 헤더 컴포넌트 불러오기

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,12 @@
 import { PropsWithChildren } from 'react';
 import GlobalStyle from 'src/styles/GlobalStyle';
+import Header from './Header/Header';
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
     <>
       <GlobalStyle />
+      <Header />
       <main>{children}</main>
     </>
   );
