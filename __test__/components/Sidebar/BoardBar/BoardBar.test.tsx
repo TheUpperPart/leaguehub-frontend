18add2e1b6950c78b5facfd79a3063be81ecf76a
pp449 <mma7710@naver.com>
2023-06-21 13:27:26 +0900

    Test: 컴포넌트에 fetch 된 데이터가 제대로 렌더링 되는지 테스트

---
+++ __test__/components/Sidebar/BoardBar/BoardBar.test.tsx
@@ -0,0 +1,36 @@
+import BoardBar from '@components/Sidebar/BoardBar/BoardBar';
+import { render, screen } from '@testing-library/react';
+
+jest.mock('@tanstack/react-query', () => ({
+  useQuery: jest.fn().mockImplementation(() => {
+    const data = {
+      hostName: 'host1',
+      leagueTitle: '부경대 총장기',
+      game: 'TFT',
+      participateNum: 999,
+      channels: [
+        {
+          id: '1',
+          name: '공지사항',
+        },
+      ],
+      permission: 0,
+    };
+    const isLoading = false;
+    const error = {};
+    return { data, isLoading, error };
+  }),
+}));
+
+describe('채널 게시판 테스트', () => {
+  it('렌더링 테스트', async () => {
+    render(<BoardBar channelId='1' />);
+    const hostName = await screen.findByText('host1');
+    const leagueTitle = await screen.findByText('부경대 총장기');
+    const game = await screen.findByText('TFT');
+
+    expect(hostName).toBeInTheDocument();
+    expect(leagueTitle).toBeInTheDocument();
+    expect(game).toBeInTheDocument();
+  });
+});
