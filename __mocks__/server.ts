9c6d6fc807c89f04a3270adeaa3331de394c360b
navyjeongs <wjdtjr8649@naver.com>
2023-06-26 18:12:33 +0900

    Feat: 프로필 가져오는 MSW 설정

--- __mocks__/server.ts
+++ __mocks__/server.ts
@@ -1,6 +1,13 @@
 import { setupServer } from 'msw/node';
-import testHandlers from '@mocks/handlers/testHandlers';
-import channelHandlers from '@mocks/handlers/channelHandlers';
-import boardHandlers from '@mocks/handlers/boardHandlers';
 
-export const server = setupServer(...testHandlers, ...channelHandlers, ...boardHandlers);
+import testHandlers from '@mocks/handlers/testHandlers';
+import boardHandlers from '@mocks/handlers/boardHandlers';
+import channelHandlers from '@mocks/handlers/channelHandlers';
+import profileHandlers from '@mocks/handlers/profileHandlers';
+
+export const server = setupServer(
+  ...testHandlers,
+  ...channelHandlers,
+  ...boardHandlers,
+  ...profileHandlers,
+);
