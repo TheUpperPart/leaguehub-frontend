230747f039c7e9fe93fef87ddf846ec5c97fc515
navyjeongs <wjdtjr8649@naver.com>
2023-06-26 18:16:19 +0900

    Feat: 프로필 가져오는 useQuery 생성 및 ProfileContext에 저장

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,17 @@
-import ChannelBar from '@components/Sidebar/ChannelBar/ChannelBar';
-import { SERVER_URL } from '@config/index';
-import styled from '@emotion/styled';
-import { useQuery } from '@tanstack/react-query';
+import { PropsWithChildren, useContext, useEffect, useState } from 'react';
 import axios from 'axios';
-import { PropsWithChildren, useState } from 'react';
-import GlobalStyle from 'src/styles/GlobalStyle';
-import Header from './Header/Header';
+import { useQuery } from '@tanstack/react-query';
+import styled from '@emotion/styled';
+
+import ChannelBar from '@components/Sidebar/ChannelBar/ChannelBar';
 import BoardBar from '@components/Sidebar/BoardBar/BoardBar';
+import Header from '@components/Header/Header';
+import { SERVER_URL } from '@config/index';
+import fetchProfile from '@apis/fetchProfile';
+import { Profile } from '@type/profile';
+import ProfileContext from '@contexts/ProfileContext';
+
+import GlobalStyle from 'src/styles/GlobalStyle';
 
 const fetchData = async () => {
   const response = await axios.get(SERVER_URL + '/api/channels', {
@@ -19,15 +24,33 @@ const fetchData = async () => {
 };
 
 const Layout = ({ children }: PropsWithChildren) => {
+  const isHaveAccessToken =
+    typeof window !== 'undefined' ? localStorage.getItem('accessToken') : null; // 액세스 토큰 있는지 확인
+
+  const profileContext = useContext(ProfileContext);
+
   const [selectedChannel, setSelectedChannel] = useState<string>('0');
   const { data } = useQuery(['getChannels'], fetchData, {
     staleTime: Infinity,
     cacheTime: Infinity,
   });
 
+  const profileQuery = useQuery<Profile>({
+    queryKey: ['getProfile'],
+    queryFn: fetchProfile,
+    enabled: isHaveAccessToken ? true : false, // 액세스 토큰이 있으면 query 요청
+  });
+
   const updateSelectedChannel = (channelId: string) => {
     setSelectedChannel(channelId);
   };
+
+  // 프로필을 가져왔을 때
+  useEffect(() => {
+    if (profileContext && profileQuery.data) {
+      profileContext.setProfile({ ...profileQuery.data });
+    }
+  }, [profileQuery.data]);
 
   return (
     <>
