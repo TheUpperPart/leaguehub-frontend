56b0659baecc394d3da709afa34ba4bce951a893
navyjeongs <wjdtjr8649@naver.com>
2023-06-26 18:11:14 +0900

    Feat: ProfileProvider로 Layout 컴포넌트 감싸기

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
-import Layout from '@components/layout';
-import initMockAPI from '@mocks/index';
-import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { AppProps } from 'next/app';
 import { useState } from 'react';
-import LoginProvider from '@components/providers/LoginProvider';
+import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import Layout from '@components/layout';
+import ProfileProvider from '@components/providers/ProfileProvider';
+
+import initMockAPI from '@mocks/index';
 
 if (process.env.NODE_ENV === 'development') {
   initMockAPI();
@@ -17,11 +19,11 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
         <ReactQueryDevtools initialIsOpen={false} />
-        <Layout>
-          <LoginProvider>
+        <ProfileProvider>
+          <Layout>
             <Component {...pageProps} />
-          </LoginProvider>
-        </Layout>
+          </Layout>
+        </ProfileProvider>
       </Hydrate>
     </QueryClientProvider>
   );
