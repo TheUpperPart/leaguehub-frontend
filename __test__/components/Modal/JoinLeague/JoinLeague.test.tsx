37a90bb6b06030b8a7d0ce6223a6278301ea62d2
Lee sang Yeop <mma7710@naver.com>
2023-07-22 01:46:48 +0900

    Test: 게임 아이디 입력하면 티어가 보이는지 테스트

--- __test__/components/Modal/JoinLeague/JoinLeague.test.tsx
+++ __test__/components/Modal/JoinLeague/JoinLeague.test.tsx
@@ -2,6 +2,7 @@ import JoinLeague from '@components/Modal/JoinLeague/JoinLeague';
 import { render, screen } from '@testing-library/react';
 import useProfile from '@hooks/useProfile';
 import { Profile } from '@type/profile';
+import userEvent from '@testing-library/user-event';
 
 const mockProfile: Profile = {
   nickname: '현석',
@@ -20,9 +21,25 @@ describe('리그 참여하는 모달 테스트', () => {
       profile: mockProfile,
     });
   });
+
   it('사용중인 프로필이 있는경우 프로필 이름 사용 테스트', async () => {
     render(<JoinLeague />);
     const userName = await screen.findByText('현석');
     expect(userName).toBeInTheDocument();
+  });
+
+  it('게임 아이디 입력 시 티어 표시 테스트', async () => {
+    render(<JoinLeague />);
+
+    const gameId = screen.getByPlaceholderText('게임 아이디');
+
+    const gameIdSearch = await screen.findByRole('button', {
+      name: '입력',
+    });
+
+    await userEvent.type(gameId, '비취골렘');
+    await userEvent.click(gameIdSearch);
+
+    expect(screen.getByText('브론즈 1')).toBeInTheDocument();
   });
 });
