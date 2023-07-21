37d874e3b446ffeabd6361edd0fbd9c3d4cfa634
Lee sang Yeop <mma7710@naver.com>
2023-07-22 01:46:26 +0900

    Test: MSW 코드 오류 수정 return, res 추가

--- __mocks__/handlers/statHandlers.ts
+++ __mocks__/handlers/statHandlers.ts
@@ -5,9 +5,9 @@ const statHandlers = [
   rest.get(SERVER_URL + '/api/stat', (req, res, ctx) => {
     const query = req.url.searchParams;
     if (query.get('gameid') === '비취골렘') {
-      ctx.json({ tier: '브론즈 1' });
+      return res(ctx.status(200), ctx.json({ tier: '브론즈 1' }));
     }
-    ctx.json({ tier: '골드 3' });
+    return res(ctx.status(200), ctx.json({ tier: '골드 3' }));
   }),
 ];
 
