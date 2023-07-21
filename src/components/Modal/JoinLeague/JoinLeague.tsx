b7fa959566952f6bd2ba01b032f61fb726c66d64
Lee sang Yeop <mma7710@naver.com>
2023-07-22 01:47:06 +0900

    Feat: 게임 아이디를 입력하면 티어가 보이도록 기능 추가

--- src/components/Modal/JoinLeague/JoinLeague.tsx
+++ src/components/Modal/JoinLeague/JoinLeague.tsx
@@ -1,17 +1,47 @@
 import Modal from '@components/Modal';
+import { SERVER_URL } from '@config/index';
+import styled from '@emotion/styled';
 import useProfile from '@hooks/useProfile';
+import axios from 'axios';
+import { ChangeEvent, useState, MouseEventHandler } from 'react';
 
 const JoinLeague = () => {
+  const [gameId, setGameId] = useState<string>('');
+  const [tier, setTier] = useState<string | null>(null);
   const { profile } = useProfile();
+
+  const handleGameIdChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setGameId(e.target.value);
+  };
+
+  const submitGameId: MouseEventHandler<HTMLElement> = async () => {
+    if (gameId.length < 2) {
+      return;
+    }
+    const userTier: string = (await axios.get(SERVER_URL + '/api/stat?gameid=' + gameId)).data.tier;
+    setTier(userTier);
+  };
 
   return (
     <Modal>
       <>
         <p>이름</p>
         {profile?.nickname}
+        <p>게임 아이디</p>
+        <Input
+          type='text'
+          name='gameIdInput'
+          placeholder='게임 아이디'
+          onChange={handleGameIdChange}
+        />
+        <Button onClick={submitGameId}>입력</Button>
+        {tier && <p>{tier}</p>}
       </>
     </Modal>
   );
 };
 
 export default JoinLeague;
+
+const Input = styled.input``;
+const Button = styled.button``;
