6419e1c6b1d7c353b1fd4dbde416c46b7f035be8
Lee sang Yeop <mma7710@naver.com>
2023-07-22 13:48:22 +0900

    Feat: 참여코드 입력하는 input 태그 추가

--- src/components/Modal/JoinLeague/JoinLeague.tsx
+++ src/components/Modal/JoinLeague/JoinLeague.tsx
@@ -7,12 +7,9 @@ import { ChangeEvent, useState, MouseEventHandler } from 'react';
 
 const JoinLeague = () => {
   const [gameId, setGameId] = useState<string>('');
+  const [joinCode, setJoinCode] = useState<string>('');
   const [tier, setTier] = useState<string | null>(null);
   const { profile } = useProfile();
-
-  const handleGameIdChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setGameId(e.target.value);
-  };
 
   const submitGameId: MouseEventHandler<HTMLElement> = async () => {
     if (gameId.length < 2) {
@@ -28,14 +25,15 @@ const JoinLeague = () => {
         <p>이름</p>
         {profile?.nickname}
         <p>게임 아이디</p>
-        <Input
-          type='text'
-          name='gameIdInput'
-          placeholder='게임 아이디'
-          onChange={handleGameIdChange}
-        />
+        <Input type='text' placeholder='게임 아이디' onChange={(e) => setGameId(e.target.value)} />
         <Button onClick={submitGameId}>입력</Button>
         {tier && <p>{tier}</p>}
+        <p>참여 코드</p>
+        <Input
+          type='text'
+          placeholder='코드번호'
+          onChange={(e) => setJoinCode(e.target.value)}
+        ></Input>
       </>
     </Modal>
   );
