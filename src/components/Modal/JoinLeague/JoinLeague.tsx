8fdb4cbc3002394178b65f698537d8a11711370e
Lee sang Yeop <mma7710@naver.com>
2023-07-28 21:10:04 +0900

    Design: Flex를 이용해서 컨텐츠 정렬

--- src/components/Modal/JoinLeague/JoinLeague.tsx
+++ src/components/Modal/JoinLeague/JoinLeague.tsx
@@ -3,9 +3,10 @@ import { SERVER_URL } from '@config/index';
 import styled from '@emotion/styled';
 import useProfile from '@hooks/useProfile';
 import axios from 'axios';
-import { ChangeEvent, useState, MouseEventHandler } from 'react';
+import { ChangeEvent, useState, MouseEventHandler, useEffect } from 'react';
 
 const JoinLeague = () => {
+  const [nickname, setNickname] = useState<string>();
   const [gameId, setGameId] = useState<string>('');
   const [joinCode, setJoinCode] = useState<string>('');
   const [tier, setTier] = useState<string | null>(null);
@@ -20,35 +21,85 @@ const JoinLeague = () => {
     setTier(userTier);
   };
 
+  useEffect(() => {
+    if (profile) setNickname(profile.nickname);
+  }, []);
+
   return (
     <Modal>
-      <>
-        <p>이름</p>
-        {profile?.nickname}
-        <p>게임 아이디</p>
-        <Input
-          type='text'
-          placeholder='게임 아이디'
-          onChange={(e: ChangeEvent<HTMLInputElement>) => setGameId(e.target.value)}
-        />
-        <Button onClick={submitGameId}>입력</Button>
-        {tier && <p>{tier}</p>}
-        <p>참여 코드</p>
-        <Input
-          type='text'
-          placeholder='코드번호'
-          onChange={(e: ChangeEvent<HTMLInputElement>) => setJoinCode(e.target.value)}
-        ></Input>
-        <Input type='checkbox' id='confirmJoin' onClick={() => setChecked(!checked)} />
-        <label htmlFor='confirmJoin'>신청 하시겠습니까?</label>
-        <Button>취소</Button>
-        <Button disabled={!checked}>신청</Button>
-      </>
+      <Container>
+        <Wrapper>
+          <FlexWrapper>이름</FlexWrapper>
+          <FlexWrapper>
+            {nickname ? (
+              nickname
+            ) : (
+              <>
+                <Input
+                  type='text'
+                  placeholder='닉네임'
+                  onChange={(e: ChangeEvent<HTMLInputElement>) => setNickname(e.target.value)}
+                />
+                <Button onClick={() => setNickname()}>수정</Button>
+              </>
+            )}
+          </FlexWrapper>
+        </Wrapper>
+        <Wrapper>
+          <FlexWrapper>게임 아이디</FlexWrapper>
+          <FlexWrapper>
+            <Input
+              type='text'
+              placeholder='게임 아이디'
+              onChange={(e: ChangeEvent<HTMLInputElement>) => setGameId(e.target.value)}
+            />
+            <Button onClick={submitGameId}>입력</Button>
+          </FlexWrapper>
+        </Wrapper>
+        <Wrapper>
+          <FlexWrapper>현재 티어</FlexWrapper>
+          <FlexWrapper>{tier && <div>{tier}</div>}</FlexWrapper>
+        </Wrapper>
+        <Wrapper>
+          <FlexWrapper>참여 코드</FlexWrapper>
+          <FlexWrapper>
+            <Input
+              type='text'
+              placeholder='코드번호'
+              onChange={(e: ChangeEvent<HTMLInputElement>) => setJoinCode(e.target.value)}
+            ></Input>
+          </FlexWrapper>
+        </Wrapper>
+        <Wrapper>
+          <Input type='checkbox' id='confirmJoin' onClick={() => setChecked(!checked)} />
+          <label htmlFor='confirmJoin'>신청 하시겠습니까?</label>
+        </Wrapper>
+        <Wrapper>
+          <Button>취소</Button>
+          <Button disabled={!checked}>신청</Button>
+        </Wrapper>
+      </Container>
     </Modal>
   );
 };
 
 export default JoinLeague;
+
+const Container = styled.div`
+  color: black;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+const FlexWrapper = styled.div`
+  flex: 1;
+  justify-content: flex-start;
+`;
 
 const Input = styled.input``;
 const Button = styled.button``;
