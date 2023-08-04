eace778c318e848c2a5210dd7364aab7a31edd83
Lee sang Yeop <mma7710@naver.com>
2023-08-04 21:06:00 +0900

    Style: 콘텐츠 수정 버튼과 디자인 추가

--- src/pages/contents/[channelLink]/[boardId].tsx
+++ src/pages/contents/[channelLink]/[boardId].tsx
@@ -1,4 +1,6 @@
 import authAPI from '@apis/authAPI';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
@@ -27,7 +29,43 @@ const boardContents = () => {
     fetchBoardContent(channelLinkString, boardIdString);
   }, [channelLink, boardId]);
 
-  return <ReactMarkdown children={contents} />;
+  return (
+    <Container>
+      <div
+        css={css`
+          padding-bottom: 1rem;
+        `}
+      >
+        <ReactMarkdown children={contents} />
+      </div>
+      <ModifyButton>내용 수정</ModifyButton>
+    </Container>
+  );
 };
 
 export default boardContents;
+
+const Container = styled.div`
+  padding: 5rem;
+  font-size: 2rem;
+  position: relative;
+  max-height: 85vh;
+  overflow: auto;
+  padding-bottom: 5rem;
+`;
+
+const ModifyButton = styled.button`
+  font-size: 2rem;
+  color: white;
+  background-color: #0067a3;
+  position: fixed;
+  bottom: 3rem;
+  right: 5rem;
+  border: none;
+  padding: 1rem;
+  border-radius: 1rem;
+
+  &: hover {
+    cursor: pointer;
+  }
+`;
