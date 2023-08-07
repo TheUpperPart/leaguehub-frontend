abe0d2ccffa1f153b824dea8ed418c4c608062ab
navyjeongs <wjdtjr8649@naver.com>
2023-08-07 11:22:56 +0900

    Feat: 입력한 정보 서버에 전송

--- src/pages/make-channel.tsx
+++ src/pages/make-channel.tsx
@@ -1,10 +1,12 @@
-import { useContext, useState } from 'react';
 import { keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
 
 import SelectGame from '@components/MakeChannel/SelectGame';
+import SelectRule from '@components/MakeChannel/SelectRule';
 import Button from '@components/Button/index';
 import useMakeGame from '@hooks/useMakeGame';
+import authAPI from '@apis/authAPI';
+import { useRouter } from 'next/router';
 
 export interface RuleSetting {
   category: number;
@@ -21,18 +23,43 @@ export interface RuleSetting {
 }
 
 const MakeChannel = () => {
-  const { currentStep, handleCurrentStep } = useMakeGame();
+  const router = useRouter();
+
+  const { currentStep, handleCurrentStep, resetState } = useMakeGame();
+
+  const fetchMakeGame = async () => {
+    try {
+      const res = await authAPI({
+        method: 'post',
+        url: '/api/channel',
+      });
+
+      if (res.status === 200) {
+        resetState();
+        router.push('/');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <Container>
-      <Wrapper>{currentStep === 0 && <SelectGame />}</Wrapper>
-      {currentStep !== 0 && (
-        <StepBtnContainer>
+      <Wrapper>
+        {currentStep === 0 && <SelectGame />}
+        {currentStep === 1 && <SelectRule />}
+      </Wrapper>
+      <StepBtnContainer>
+        {currentStep === 0 ? (
           <Button width={25} height={7} borderRadius={1} onClick={handleCurrentStep}>
             다음
           </Button>
-        </StepBtnContainer>
-      )}
+        ) : (
+          <Button width={25} height={7} borderRadius={1} onClick={fetchMakeGame}>
+            생성하기
+          </Button>
+        )}
+      </StepBtnContainer>
     </Container>
   );
 };
