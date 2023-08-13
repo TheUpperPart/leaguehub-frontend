e052d961f803c5d8af3def306fb612ecd403b507
navyjeongs <wjdtjr8649@naver.com>
2023-08-13 23:34:02 +0900

    Feat: 이메일 유효성 검사 함수 구현

---
+++ src/utils/checkEmailValidation.ts
@@ -0,0 +1,11 @@
+const checkEmailValidation = (emailStr: string) => {
+  const regExp = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]+$/;
+
+  if (regExp.test(emailStr)) {
+    return true;
+  }
+
+  return false;
+};
+
+export default checkEmailValidation;
