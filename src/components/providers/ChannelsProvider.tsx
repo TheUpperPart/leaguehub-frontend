a7d380ea764232f4693afec84ba2f3d5cc044978
navyjeongs <wjdtjr8649@naver.com>
2023-08-15 00:02:27 +0900

    Feat: Channels를 전역으로 관리하기 위한 Provider 설정

---
+++ src/components/providers/ChannelsProvider.tsx
@@ -0,0 +1,55 @@
+import { useQuery } from '@tanstack/react-query';
+import { useState, useEffect } from 'react';
+import Cookies from 'js-cookie';
+
+import { ChannelCircleProps } from '@type/channelCircle';
+import ChannelsContext from '@contexts/ChannelsContext';
+import authAPI from '@apis/authAPI';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const fetchChannels = async () => {
+  const res = await authAPI<ChannelCircleProps[]>({
+    method: 'get',
+    url: '/api/channels',
+  });
+
+  return res.data;
+};
+
+const ChannelsProvider = ({ children }: Props) => {
+  const isHaveAccessToken = Cookies.get('accessToken');
+
+  const [channels, setChannels] = useState<ChannelCircleProps[]>([]);
+
+  const { data } = useQuery<ChannelCircleProps[]>({
+    queryKey: ['getChannels'],
+    queryFn: fetchChannels,
+    enabled: isHaveAccessToken ? true : false,
+  });
+
+  const addChannel = (channel: ChannelCircleProps) => {
+    setChannels([...channels, channel]);
+  };
+
+  const removeChannel = (channelLink: string) => {
+    const filterChannels = [...channels].filter((channel) => channel.channelLink !== channelLink);
+    setChannels(filterChannels);
+  };
+
+  useEffect(() => {
+    if (data) {
+      setChannels([...data]);
+    }
+  }, [data]);
+
+  return (
+    <ChannelsContext.Provider value={{ channels, addChannel, removeChannel }}>
+      {children}
+    </ChannelsContext.Provider>
+  );
+};
+
+export default ChannelsProvider;
