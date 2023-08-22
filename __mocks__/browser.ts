252240da1b17c459a280062f23140a7d64aec152
navyjeongs <wjdtjr8649@naver.com>
2023-08-23 01:18:52 +0900

    Test: 채널의 라운드 정보에 대한 mocking 데이터 추가

--- __mocks__/browser.ts
+++ __mocks__/browser.ts
@@ -7,6 +7,7 @@ import profileHandlers from '@mocks/handlers/profileHandlers';
 import statHandlers from '@mocks/handlers/statHandlers';
 import makeGameHandlers from './handlers/makeGameHandlers';
 import mypageHandlers from './handlers/mypageHandlers';
+import bracketHandlers from './handlers/bracketHandlers';
 
 export const worker = setupWorker(
   ...testHandlers,
@@ -16,4 +17,5 @@ export const worker = setupWorker(
   ...statHandlers,
   ...makeGameHandlers,
   ...mypageHandlers,
+  ...bracketHandlers,
 );
