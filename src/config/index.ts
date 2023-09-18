aa4e483659e9f8ef15416e6d00f68363b643f5f5
Lee sang Yeop <mma7710@naver.com>
2023-09-18 21:59:32 +0900

    Config: 소켓 주소 추가

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,8 @@
 let SERVER_URL = 'http://54.180.1.85:8080';
+const SOCKET_URL = 'ws://54.180.1.85:8080/ws/websocket';
 
 if (process.env.NODE_ENV === 'development') {
   SERVER_URL = 'http://localhost:8080';
 }
 
-export { SERVER_URL };
+export { SERVER_URL, SOCKET_URL };
