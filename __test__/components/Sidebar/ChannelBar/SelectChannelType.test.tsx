410fb65752deedb354ae6f9514159b533eebecbd
navyjeongs <wjdtjr8649@naver.com>
2023-10-05 20:03:34 +0900

    Feat: 대회 개최.참여 단게 state 생성
    대회 개최 단계를 저장하는 것을 Context에서 삭제하고 useState로
    관리

--- __test__/components/Sidebar/ChannelBar/SelectChannelType.test.tsx
+++ __test__/components/Sidebar/ChannelBar/SelectChannelType.test.tsx
@@ -1,45 +1,45 @@
-import Modal from '@components/Modal';
 import SelectChannelType from '@components/Sidebar/ChannelBar/SelectChannelType';
 import { render, screen } from '@testing-library/react';
-import mockRouter from 'next-router-mock';
 import userEvent from '@testing-library/user-event';
 import ChannelsProvider from '@components/providers/ChannelsProvider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-jest.mock('next/router', () => require('next-router-mock'));
+import MakeGameProvider from '@components/providers/MakeGameProvider';
 
 describe('채널 추가 테스트', () => {
   const queryClient = new QueryClient();
-
-  const mockFn = jest.fn();
 
   it('초기에는 대회 개최 버튼과 대회 참여 버튼이 있다.', () => {
     render(
       <QueryClientProvider client={queryClient}>
         <ChannelsProvider>
-          <SelectChannelType
-            handleModal={() => {
-              return;
-            }}
-          />
+          <MakeGameProvider>
+            <SelectChannelType
+              handleModal={() => {
+                return;
+              }}
+            />
+          </MakeGameProvider>
         </ChannelsProvider>
       </QueryClientProvider>,
     );
 
     const btns = screen.getAllByRole('button');
+
     expect(btns[0].innerHTML).toBe('대회 개최');
-    expect(btns[1].innerHTML).toBe('대회 참여');
+    expect(btns[1].innerHTML).toBe('채널 참여');
   });
 
-  it('대회 개최하기 버튼을 누르면 make-channel 페이지로 이동한다.', async () => {
+  it('대회 개최하기 버튼을 누르면 게임을 선택하는 화면이 나타난다..', async () => {
     render(
       <QueryClientProvider client={queryClient}>
         <ChannelsProvider>
-          <SelectChannelType
-            handleModal={() => {
-              return;
-            }}
-          />
+          <MakeGameProvider>
+            <SelectChannelType
+              handleModal={() => {
+                return;
+              }}
+            />
+          </MakeGameProvider>
         </ChannelsProvider>
       </QueryClientProvider>,
     );
@@ -47,23 +47,26 @@ describe('채널 추가 테스트', () => {
     const makeBtn = screen.getByText('대회 개최');
     await userEvent.click(makeBtn);
 
-    expect(mockRouter.asPath).toBe('/make-channel');
+    const title = screen.getByText('개최하실 게임을 선택해주세요.');
+    expect(title).toBeInTheDocument();
   });
 
-  it('대회 참여하기 버튼을 누르면 참여 코드를 입력하는 input이 표시된다.', async () => {
+  it('채널 참여하기 버튼을 누르면 참여 코드를 입력하는 input이 표시된다.', async () => {
     render(
       <QueryClientProvider client={queryClient}>
         <ChannelsProvider>
-          <SelectChannelType
-            handleModal={() => {
-              return;
-            }}
-          />
+          <MakeGameProvider>
+            <SelectChannelType
+              handleModal={() => {
+                return;
+              }}
+            />
+          </MakeGameProvider>
         </ChannelsProvider>
       </QueryClientProvider>,
     );
 
-    const enterBtn = screen.getByText('대회 참여');
+    const enterBtn = screen.getByText('채널 참여');
     await userEvent.click(enterBtn);
 
     const input = screen.getByPlaceholderText('참여 코드 입력');
