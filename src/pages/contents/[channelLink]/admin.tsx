1639514a01edf5ccdfd08f11554c64c3a085f72c
navyjeongs <wjdtjr8649@naver.com>
2023-10-10 11:01:27 +0900

    Feat: admin 페이지 접근시 최초 권한 확인

--- src/pages/contents/[channelLink]/admin.tsx
+++ src/pages/contents/[channelLink]/admin.tsx
@@ -1,14 +1,17 @@
-import authAPI from '@apis/authAPI';
-import Button from '@components/Button';
-import Modal from '@components/Modal';
-import ModifyChannel from '@components/Modal/ModifyChannel/ModifyChannel';
-import { SERVER_URL } from '@config/index';
-import styled from '@emotion/styled';
-import useModals from '@hooks/useModals';
 import axios from 'axios';
-import { GetServerSideProps } from 'next';
+import styled from '@emotion/styled';
 import { useRouter } from 'next/router';
+import { GetServerSideProps } from 'next';
 
+import Modal from '@components/Modal';
+import Button from '@components/Button';
+import ModifyBracket from '@components/Modal/ModifyChannel/ModifyBracket';
+import ModifyChannel from '@components/Modal/ModifyChannel/ModifyChannel';
+
+import useModals from '@hooks/useModals';
+
+import { SERVER_URL } from '@config/index';
+import { parse } from 'cookie';
 interface Props {
   role: string;
 }
@@ -18,43 +21,28 @@ const Admin = ({ role }: Props) => {
 
   const { openModal, closeModal } = useModals();
 
-  const fetchNextBracket = async () => {
-    const res = await authAPI({
-      method: 'post',
-      url: `/api/match/${router.query.channelLink as string}/1`,
-    });
-  };
-
-  const fetchStartBracket = async () => {
-    const res = await authAPI({
-      method: 'put',
-      url: `/api/channel/${router.query.channelLink as string}&status=1`,
-    });
-  };
-
-  const handleStartBracket = async () => {
-    if (window.confirm('대회를 시작하시겠습니까?\n대회 시작 후 중지할 수 없습니다!')) {
-      fetchStartBracket();
-    }
-  };
-
   if (!role) {
     router.push('/');
   }
 
   return (
     <Container>
-      <Header>대회 매치 설정</Header>
+      <Header>대회 설정</Header>
       <BracketContainer>
-        <Button width={20} height={6} onClick={handleStartBracket}>
-          대회 시작하기
-        </Button>
-
-        <Button width={20} height={6} onClick={fetchNextBracket}>
-          라운드 배정하기
+        <Button
+          width={20}
+          height={6}
+          onClick={() =>
+            openModal(Modal, {
+              onClose: () => closeModal(Modal),
+              children: <ModifyBracket onClose={() => closeModal(Modal)} />,
+            })
+          }
+        >
+          대회 관리하기
         </Button>
       </BracketContainer>
-      <Header>대회 정보 변경</Header>
+      <Header>채널 정보 설정</Header>
       <Button
         width={20}
         height={6}
@@ -96,9 +84,15 @@ const BracketContainer = styled.div`
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   try {
+    const cookies = parse(context.req.headers.cookie || 'no-cookie');
+    const accessToken = cookies.accessToken || undefined;
+
     const res = await axios({
       method: 'get',
       url: SERVER_URL + `/api/channel/${context.query.channelLink as string}/permission`,
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
     });
 
     return {
