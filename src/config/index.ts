8ae612602db067010b2c2739557b279a074448ac
Lee sang Yeop <mma7710@naver.com>
2023-11-21 14:09:33 +0900

    Fix(Config): 기본 서버 주소 변경

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
-let SERVER_URL = 'http://54.180.1.85:8080';
-const SOCKET_URL = 'ws://54.180.1.85:8080/ws/websocket';
+let SERVER_URL = 'http://3.38.168.158:8080';
+const SOCKET_URL = 'ws://3.38.168.158:8080/ws/websocket';
 
 if (process.env.NODE_ENV === 'development') {
   SERVER_URL = 'http://localhost:8080';
